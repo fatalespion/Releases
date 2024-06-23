9c723daad89b079ee4c2edc4791cfb826f640cf4
fatalespion <145792219+fatalespion@users.noreply.github.com>
2024-06-23 14:35:41 +0200

    Update EXPHUB.lua

--- EXPHUB.lua
+++ EXPHUB.lua
@@ -14,11 +14,24 @@ end
 
 qNVAKkuwxNpqruLjSRHg = CheckHWID(HWID)
 
+shared.user = "admin"
+shared.pass = "hFzU4cM8w2gE7C"
+
 if qNVAKkuwxNpqruLjSRHg == true then
-	if shared.pass ~= "hFzU4cM8w2gE7C" or shared.user ~= game.Players.LocalPlayer.UserId or shared.user ~= "admin" then
-		print("no")
+	if shared.pass ~= "hFzU4cM8w2gE7C" then
+		print(1)
 		return
 	end
+
+	if shared.user ~= game.Players.LocalPlayer.UserId then
+		if HWID == "22546EE2-F5D6-4271-8575-67299BEA5DCF" then
+			if shared.user ~= "admin" then
+				return
+			end  
+		else
+			return
+		end    
+	end  
 
 	local repo = 'https://raw.githubusercontent.com/'
 
@@ -53,6 +66,7 @@ if qNVAKkuwxNpqruLjSRHg == true then
 	local GunsLeftVisualGroupBox = Tabs.Visual:AddLeftGroupbox('GUN MODS')
 	local CrosshairRightVisualGroupBox = Tabs.Visual:AddRightGroupbox('CROSSHAIR')
 	local TracerLeftVisualGroupBox = Tabs.Visual:AddLeftGroupbox('TRACER')
+	local OthersLeftVisualGroupBox = Tabs.Visual:AddRightGroupbox('OTHERS')
 
 	local HasGun = false
 
@@ -889,7 +903,33 @@ if qNVAKkuwxNpqruLjSRHg == true then
 			_G.ESPDropBag = Value
 		end
 	})
+	
+	LeftVisualGroupBox:AddToggle('NameTags', {
+		Text = 'Nametags',
+		Default = false, -- Default value (true / false)
+		Tooltip = 'Shows all of the players nametags', -- Information shown when you hover over the toggle
 
+		Callback = function(Value)
+			if Value == false then
+				for i,v in pairs(game.Workspace.Debris.Guis:GetChildren()) do
+					v.MaxDistance = 20
+					v.Size = UDim2.new(2.5,0, 0.9,0)
+					v.SizeOffset = UDim2.new(0, 3.75)
+					v.StudsOffset = Vector3.new(0,0,1)
+					v.AlwaysOnTop = false
+				end
+			elseif Value == true then
+				for i,v in pairs(game.Workspace.Debris.Guis:GetChildren()) do
+					v.MaxDistance = 999999999999
+					v.Size = UDim2.new(0,200, 0,30)
+					v.SizeOffset = UDim2.new(0, 0)
+					v.StudsOffset = Vector3.new(0,0,0)
+					v.AlwaysOnTop = true
+				end
+			end
+		end
+	})
+	
 	LeftVisualGroupBox:AddDivider()
 
 	LeftVisualGroupBox:AddLabel('Tracer Color'):AddColorPicker('ESPColorPicker', {
@@ -1123,6 +1163,23 @@ if qNVAKkuwxNpqruLjSRHg == true then
 			end
 		end
 	end)
+	
+	OthersLeftVisualGroupBox:AddToggle('BFullBright', {
+		Text = 'Fullbright',
+		Default = false,
+		Tooltip = 'be a bat',
+		Callback = function(Value)
+			if Value == false then
+				local ColorCorrection = Instance.new("ColorCorrectionEffect")
+				ColorCorrection.Brightness = 0.1
+				ColorCorrection.Parent = Camera
+				ColorCorrection.Name = "fullbrightness"
+			elseif Value == true then
+				Camera:FindFirstChild("fullbrightness"):Destroy()
+			end
+		end
+	})
+
 
 	local LeftGroupBox = Tabs.Debugging:AddLeftGroupbox('Remotes')
 
