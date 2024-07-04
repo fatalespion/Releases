2de6097b64e67b2a5517d70db88a13994763ce62
fatalespion <145792219+fatalespion@users.noreply.github.com>
2024-07-04 20:00:36 +0200

    Update PlayerAimbot.lua

--- PlayerAimbot.lua
+++ PlayerAimbot.lua
@@ -6,24 +6,6 @@ local TweenService = game:GetService("TweenService")
 local LocalPlayer = Players.LocalPlayer
 local Holding = false
 
-_G.AimbotEnabled = true
-_G.TeamCheck = false
-_G.AimPart = "Head"
-_G.Sensitivity = 0.1
-_G.CircleSides = 64
-_G.CircleColor = Color3.fromRGB(255, 255, 255)
-_G.CircleTransparency = 0.7
-_G.CircleRadius = 80
-_G.CircleFilled = false
-_G.CircleVisible = true
-_G.CircleThickness = 1
-
-_G.BlacklistedPlayers = {}  -- Table where player names are keys and values are true
-_G.FriendsBlacklist = true
-_G.AimKeybind = Enum.UserInputType.MouseButton2
-_G.AliveCheck = true
-_G.WallCheck = true
-
 local function IsPlayerAlive(player)
     return player.Character and player.Character:FindFirstChild("Humanoid") and player.Character.Humanoid.Health > 0
 end
@@ -56,43 +38,35 @@ local function IsPlayerInFront(localPlayer, targetPlayer)
     return false
 end
 
-local FOVCircle = Drawing.new("Circle")
-FOVCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
-FOVCircle.Radius = _G.CircleRadius
-FOVCircle.Filled = _G.CircleFilled
-FOVCircle.Color = _G.CircleColor
-FOVCircle.Visible = _G.CircleVisible
-FOVCircle.Transparency = _G.CircleTransparency
-FOVCircle.NumSides = _G.CircleSides
-FOVCircle.Thickness = _G.CircleThickness
-
 local function GetClosestPlayer()
     local MaximumDistance = _G.CircleRadius
     local Target = nil
     local MouseLocation = UserInputService:GetMouseLocation()
 
-    for _, v in ipairs(Players:GetPlayers()) do
-        if v ~= LocalPlayer and not _G.BlacklistedPlayers[v.Name] and not (_G.FriendsBlacklist and LocalPlayer:IsFriendsWith(v.UserId)) then
-            if _G.TeamCheck and v.Team == LocalPlayer.Team then
-                continue
-            end
-
-            if not _G.AliveCheck or IsPlayerAlive(v) then
-                if v.Character and v.Character:FindFirstChild(_G.AimPart) then
-                    local ScreenPoint = Camera:WorldToScreenPoint(v.Character[_G.AimPart].Position)
-                    local VectorDistance = (MouseLocation - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
-
-                    if VectorDistance < MaximumDistance and IsPlayerInFront(LocalPlayer, v) then
-                        if not _G.WallCheck or not IsPlayerBehindWall(v) then
-                            Target = v
-                            MaximumDistance = VectorDistance
+    if _G.PlayerTrack then
+        for _, v in ipairs(Players:GetPlayers()) do
+            if v ~= LocalPlayer and not _G.BlacklistedPlayers[v.Name] and not (_G.FriendsBlacklist and LocalPlayer:IsFriendsWith(v.UserId)) then
+                if _G.TeamCheck and v.Team == LocalPlayer.Team then
+                    continue
+                end
+    
+                if not _G.AliveCheck or IsPlayerAlive(v) then
+                    if v.Character and v.Character:FindFirstChild(_G.AimPart) then
+                        local ScreenPoint = Camera:WorldToScreenPoint(v.Character[_G.AimPart].Position)
+                        local VectorDistance = (MouseLocation - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+    
+                        if VectorDistance < MaximumDistance and IsPlayerInFront(LocalPlayer, v) then
+                            if not _G.WallCheck or not IsPlayerBehindWall(v) then
+                                Target = v
+                                MaximumDistance = VectorDistance
+                            end
                         end
                     end
                 end
             end
         end
-    end
-
+    end   
+    
     return Target
 end
 
