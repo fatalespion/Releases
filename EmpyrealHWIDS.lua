16758d76533d955be0220f6de7e38541d8fdec11
fatalespion <145792219+fatalespion@users.noreply.github.com>
2024-12-07 20:52:55 +0100

    Create EmpyrealHWIDS.lua

---
+++ EmpyrealHWIDS.lua
@@ -0,0 +1,17 @@
+local HWIDS = {}
+
+function HWIDS.getDevelopers()
+	return {
+		"yomaxify",
+		"jahibii",
+		"L3ARNT",
+	}
+end
+
+function HWIDS.KEYHWID()
+	return {
+		["22546EE2-F5D6-4271-8575-67299BEA5DCF"] = "admin",
+	}
+end
+
+return HWIDS
