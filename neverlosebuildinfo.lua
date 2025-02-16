2d02a4beada43e7642e56e63890829d1f9b51668
fatalespion <145792219+fatalespion@users.noreply.github.com>
2025-02-16 22:27:53 +0100

    Update neverlosebuildinfo.lua

--- neverlosebuildinfo.lua
+++ neverlosebuildinfo.lua
@@ -1,19 +1,19 @@
 local BuildInfo = {}
 
-BuildInfo.VersionType = funcion()
-    return "V1.0"
-end)
+BuildInfo.VersionType = function()
+	return "V1.0"
+end
 
-BuildInfo.GetBuild = funcion()
-    return "16/02/2025"
-end)
+BuildInfo.GetBuild = function()
+	return "16/02/2025"
+end
 
-BuildInfo.BuildType = funcion()
-    return "0.0.1"
-end)
+BuildInfo.BuildType = function()
+	return "0.0.1"
+end
 
-BuildInfo.GetNews = funcion()
-    return "NO NEWS"
-end)
+BuildInfo.GetNews = function()
+	return "NO NEWS"
+end
 
 return BuildInfo
